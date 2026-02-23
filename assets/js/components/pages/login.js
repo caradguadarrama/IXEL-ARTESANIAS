8b79b741f0955d0b69323e902a73d760e29b2046
Diana <dianaibarra0303@gmail.com>
2026-02-23 16:41:52 -0600

    se modificó el js, validacion de registro

--- assets/js/components/pages/login.js
+++ assets/js/components/pages/login.js
@@ -23,6 +23,8 @@ function handleForm(formSelector, label) {
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
 
+        localStorage.setItem(label, JSON.stringify(data));
+
         console.log(label);
         console.log(JSON.stringify(data, null, 2));
     });
@@ -42,3 +44,56 @@ if(passwordInput){
     }
     });
 }
+
+//guardar info y que el registro e inicio de sesión sea funcional 
+
+const registerForm = document.querySelector('.form2');
+
+registerForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const formData = new FormData(registerForm);
+    const data = Object.fromEntries(formData.entries());
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const exists = users.find(user => user.email === data.email);
+
+    if (exists) {
+        alert("El usuario ya existe");
+        return;
+    }
+    users.push(data);
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+    console.log("Usuarios guardados:", users);
+
+    alert("Registro exitoso");
+
+    toggleForm(); // volver a login
+});
+
+const loginForm = document.querySelector('.form');
+
+loginForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const formData = new FormData(loginForm);
+    const data = Object.fromEntries(formData.entries());
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const user = users.find(user => user.email === data.email);
+
+    if (!user || user.password !== data.password) {
+        alert("Credenciales incorrectas");
+        return;
+    }
+    localStorage.setItem("currentUser", JSON.stringify(user)); // guardar sesión
+
+    console.log("Usuario logueado:", user);
+    window.location.href = "/index.html";// REDIRECCIÓN
+});
